d0f4eb8b0adfcceaa5e79444ca11dc4e31b86ac8
nickf723 <nickfarley723@gmail.com>
2026-07-18 21:58:39 -0400

    Run thermal regression with circuit suite

--- scripts/testing/conductive_network_test_fixture.gd
+++ scripts/testing/conductive_network_test_fixture.gd
@@ -108,6 +108,7 @@ static func run(host: Node) -> Array[String]:
 	fixture.queue_free()
 	failures.append_array(await ElectricalInteroperabilityTestFixture.run(host))
 	failures.append_array(await ConductiveWaterTestFixture.run(host))
+	failures.append_array(await ThermalStateTestFixture.run(host))
 	return failures
 
 

86a01e04fad9678af0eb3f112d2baf298e470ea0
nickf723 <nickfarley723@gmail.com>
2026-07-25 13:48:56 -0400

    Make species knowledge smoke test autoload-safe

--- scripts/tests/species_knowledge_smoke_test.gd
+++ scripts/tests/species_knowledge_smoke_test.gd
@@ -1,17 +1,23 @@
 extends Node
 
 var failures: Array[String] = []
+var species_knowledge: Node
 
 
 func _ready() -> void:
-	SpeciesKnowledge.reset_species("goose")
-	var first: Dictionary = SpeciesKnowledge.add_discovery("goose", "walking_gait", "Walking gait", 2)
+	species_knowledge = get_node_or_null("/root/SpeciesKnowledge")
+	_expect(species_knowledge != null, "SpeciesKnowledge autoload is available")
+	if species_knowledge == null:
+		_finish()
+		return
+	species_knowledge.call("reset_species", "goose")
+	var first: Dictionary = species_knowledge.call("add_discovery", "goose", "walking_gait", "Walking gait", 2)
 	_expect(bool(first.get("new_discovery", false)), "First observation records")
-	var duplicate: Dictionary = SpeciesKnowledge.add_discovery("goose", "walking_gait", "Walking gait", 2)
+	var duplicate: Dictionary = species_knowledge.call("add_discovery", "goose", "walking_gait", "Walking gait", 2)
 	_expect(not bool(duplicate.get("new_discovery", true)), "Duplicate observation does not farm knowledge")
-	SpeciesKnowledge.add_discovery("goose", "preferred_food", "Preferred food", 2)
-	_expect(SpeciesKnowledge.get_rank("goose") >= 1, "Knowledge rank increases")
-	_expect(SpeciesKnowledge.has_unlock("goose", "goose_familiar"), "Goose familiar unlock is earned")
+	species_knowledge.call("add_discovery", "goose", "preferred_food", "Preferred food", 2)
+	_expect(int(species_knowledge.call("get_rank", "goose")) >= 1, "Knowledge rank increases")
+	_expect(bool(species_knowledge.call("has_unlock", "goose", "goose_familiar")), "Goose familiar unlock is earned")
 	_finish()
 
 

101471e4d5460f6bd5d170eb88438313faa82d35
nickf723 <nickfarley723@gmail.com>
2026-07-31 18:08:22 -0400

    Prune stale creature observation packs

--- scripts/animals/creature_observation_runtime_service.gd
+++ scripts/animals/creature_observation_runtime_service.gd
@@ -1,5 +1,21 @@
 extends "res://scripts/animals/creature_observation_service.gd"
 
+@export_range(0.5, 10.0, 0.5) var stale_pack_prune_interval: float = 2.0
+
+var stale_pack_prune_remaining: float = 0.0
+
+
+func _process(delta: float) -> void:
+	super._process(delta)
+	stale_pack_prune_remaining = maxf(
+		stale_pack_prune_remaining - maxf(delta, 0.0),
+		0.0
+	)
+	if stale_pack_prune_remaining > 0.0:
+		return
+	stale_pack_prune_remaining = maxf(stale_pack_prune_interval, 0.5)
+	_prune_stale_packs()
+
 
 func report_creature_defeated(creature: Node) -> Dictionary:
 	if not is_instance_valid(creature):
@@ -85,6 +101,11 @@ func can_player_witness(creature: Node) -> bool:
 	return _has_line_of_sight(player, actor, aim_point, camera.global_position)
 
 
+func clear_runtime_state() -> void:
+	super.clear_runtime_state()
+	stale_pack_prune_remaining = 0.0
+
+
 func _evaluate_pack_defeat(species_id: String, pack_key: String) -> void:
 	var maximum_members: int = int(pack_max_members.get(pack_key, 0))
 	if maximum_members < 2:
@@ -109,3 +130,27 @@ func _evaluate_pack_defeat(species_id: String, pack_key: String) -> void:
 	)
 	pack_member_ids.erase(pack_key)
 	pack_max_members.erase(pack_key)
+
+
+func _prune_stale_packs() -> void:
+	for pack_key_value: Variant in pack_member_ids.keys():
+		var pack_key: String = str(pack_key_value)
+		var members_value: Variant = pack_member_ids.get(pack_key, {})
+		if not members_value is Dictionary:
+			pack_member_ids.erase(pack_key)
+			pack_max_members.erase(pack_key)
+			continue
+		var members: Dictionary = members_value as Dictionary
+		var living_ids: Dictionary = {}
+		for actor_id_value: Variant in members.keys():
+			var actor_id: int = int(actor_id_value)
+			var actor_value: Object = instance_from_id(actor_id)
+			if actor_value is Node and is_instance_valid(actor_value):
+				var actor: Node = actor_value as Node
+				if not actor.is_queued_for_deletion():
+					living_ids[actor_id] = true
+		if living_ids.is_empty():
+			pack_member_ids.erase(pack_key)
+			pack_max_members.erase(pack_key)
+		else:
+			pack_member_ids[pack_key] = living_ids

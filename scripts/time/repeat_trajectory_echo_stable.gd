2d3d59d900b669be72005ecedddb31dfe1438b6b
nickf723 <nickfarley723@gmail.com>
2026-08-08 14:03:33 -0400

    Freeze physical visual shells inside Repeat trajectories

---
+++ scripts/time/repeat_trajectory_echo_stable.gd
@@ -0,0 +1,26 @@
+extends "res://scripts/time/repeat_trajectory_echo.gd"
+class_name RepeatTrajectoryEchoStable
+
+
+func _build_visual_shell() -> void:
+	super._build_visual_shell()
+	_freeze_physics_shells(visual_shell)
+
+
+func _freeze_physics_shells(node: Node) -> void:
+	if node == null:
+		return
+	if node is RigidBody3D:
+		var body := node as RigidBody3D
+		body.freeze = true
+		body.sleeping = true
+		body.linear_velocity = Vector3.ZERO
+		body.angular_velocity = Vector3.ZERO
+	for child: Node in node.get_children():
+		_freeze_physics_shells(child)
+
+
+func get_debug_data() -> Dictionary:
+	var data: Dictionary = super.get_debug_data()
+	data["physical_visual_shells_frozen"] = true
+	return data

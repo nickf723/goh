3a81ec911e0ccfbbbbac5a23fa9fbbfd223a8d60
nickf723 <nickfarley723@gmail.com>
2026-08-13 19:33:42 -0400

    Load weapon identity rules in combat v3

--- scripts/weapons/combat_weapon_controller_v3.gd
+++ scripts/weapons/combat_weapon_controller_v3.gd
@@ -1,2 +1,6 @@
 extends "res://scripts/weapons/combat_weapon_controller_v2.gd"
 class_name CombatWeaponControllerV3
+
+const MechanicalIdentityScript = preload(
+	"res://scripts/weapons/weapon_mechanical_identity_v1.gd"
+)

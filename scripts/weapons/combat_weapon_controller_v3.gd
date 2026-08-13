f6a27b86bbfe261c937bcf7b82f06920559551c5
nickf723 <nickfarley723@gmail.com>
2026-08-13 19:33:55 -0400

    Route special boomerang paths in combat v3

--- scripts/weapons/combat_weapon_controller_v3.gd
+++ scripts/weapons/combat_weapon_controller_v3.gd
@@ -4,6 +4,9 @@ class_name CombatWeaponControllerV3
 const MechanicalIdentityScript = preload(
 	"res://scripts/weapons/weapon_mechanical_identity_v1.gd"
 )
+const BoomerangPathScript = preload(
+	"res://scripts/weapons/boomerang_path_solver.gd"
+)
 
 
 func _prepare_combat_flair_attack(
@@ -13,3 +16,17 @@ func _prepare_combat_flair_attack(
 	if resolved == null or equipped_weapon == null:
 		return resolved
 	return MechanicalIdentityScript.configure_attack(resolved, equipped_weapon.weapon_class)
+
+
+func find_targets(attack: WeaponAttackDefinition) -> Array[Node]:
+	if attack == null or equipped_weapon == null:
+		return super.find_targets(attack)
+	if equipped_weapon.weapon_class != "boomerang" or not attack.extra_tags.has("boomerang_throw"):
+		return super.find_targets(attack)
+	if (
+		attack.extra_tags.has("boomerang_hook_throw")
+		or attack.extra_tags.has("boomerang_s_curve_throw")
+		or attack.extra_tags.has("boomerang_orbit_throw")
+	):
+		return BoomerangPathScript.find_targets(self, attack)
+	return super.find_targets(attack)
